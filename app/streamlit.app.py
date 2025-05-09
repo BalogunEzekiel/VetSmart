9d8b5ce31d6f9e03feb2457a749a5f8cfc40217a
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-05-09 05:35:01 +0100

    Update streamlit.app.py

--- app/streamlit.app.py
+++ app/streamlit.app.py
@@ -135,7 +135,7 @@ elif selected_page_key == "diagnosis":
     if df.empty:
         st.warning("No livestock registered yet. Please add animals to the dashboard first.")
     else:
-        animal_name = st.selectbox("Select Registered Animal", df["name"])
+        animal_name = st.selectbox("Select Registered Animal", df["Name"])
         symptoms = st.multiselect("Select observed symptoms:", ["Fever", "Coughing", "Diarrhea", "Loss of appetite", "Lameness", "Swelling"])
 
         if st.button("🧠 Predict Disease"):
