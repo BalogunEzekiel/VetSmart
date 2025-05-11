68ab7e084b1ac9960772ed042bb9febb4753c3fe
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-05-11 20:56:24 +0100

    Update streamlit.app.py

--- app/streamlit.app.py
+++ app/streamlit.app.py
@@ -402,7 +402,8 @@ def request_vet_service():
     conn.close()
 
 # ========== Main ==========
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["📊 Dashboard", "🩺 Diagnosis", "💡 Health Tips", “Vet Doc”, “Request Service”, "📝 Feedback"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["📊 Dashboard", "🩺 Diagnosis", "💡 Health Tips", "Vet Doc", "Request Service", "📝 Feedback"])
+
 with tab1:
     display_dashboard()
 with tab2:
