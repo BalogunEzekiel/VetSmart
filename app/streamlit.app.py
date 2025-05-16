7bf4becf20457eb6c3273c0668cc4ee4714fbe46
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-05-16 09:50:48 +0100

    Update streamlit.app.py

--- app/streamlit.app.py
+++ app/streamlit.app.py
@@ -268,7 +268,9 @@ def get_sqlite_connection():
     return sqlite3.connect(SQLITE_DB)
 
 # Connect to the SQLite database (creates it if it doesn't exist)
-conn = sqlite3.connect('livestock_data.db')  # replace with your actual database name
+# conn = sqlite3.connect('livestock_data.db')  # replace with your actual database name
+conn = sqlite3.connect("livestock.db", check_same_thread=False)
+
 
 # ========== Initialize Database and Tables ==========
 def initialize_database():
