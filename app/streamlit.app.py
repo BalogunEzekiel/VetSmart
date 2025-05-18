ae0ba9700cd083a5ab0ddad6ab052a4824d529f8
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-05-18 14:28:50 +0100

    Update streamlit.app.py

--- app/streamlit.app.py
+++ app/streamlit.app.py
@@ -65,9 +65,9 @@ if not st.session_state['logged_in']:
         st.subheader("Login")
         login_user = st.text_input("Ema]il", key="login_user")
         login_pwd  = st.text_input("Password", type="password", key="login_pwd")
-#        if st.button("Login", key="login_btn"):
-        def get_sqlite_connection():
-            return sqlite3.connect("livestock_data.db")
+        if st.button("Login", key="login_btn"):
+            def get_sqlite_connection():
+                return sqlite3.connect("livestock_data.db")
     # Get database connection and cursor
             
         if st.button("Login", key="login_btn"):
